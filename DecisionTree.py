1025bfbafe463af8e7289fb5a3d3c635f23a166f
Alexander Lien <alexclien@Alexanders-MacBook-Air.local>
2026-09-04 02:42:55 -0700

    implement first decision tree classifier

---
+++ DecisionTree.py
@@ -0,0 +1,16 @@
+from sklearn import tree
+
+# grade scores
+X = [[49], [50], [79], [80], [99], [100], [12],[18],[59],[60]]
+# pass or fail
+Y = ['fail', 'fail', 'pass', 'pass', 'pass', 'pass', 'fail','fail','fail','pass']
+
+# Creates a decision tree classifier and fits the model to the data
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(X, Y)
+
+prediction_pass = clf.predict([[70]])
+prediction_fail = clf.predict([[30]])
+
+print(prediction_pass)  # Output: ['pass']
+print(prediction_fail)  # Output: ['fail']
