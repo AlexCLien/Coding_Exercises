a0cf679e9c07dd3e86c0246398bd5e99fbbe2a32
Alexander Lien <alexclien@Alexanders-MacBook-Air.local>
2026-09-05 01:15:34 -0700

    Create exercise goals

---
+++ Fundamentals/TopWord.py
@@ -0,0 +1,6 @@
+'''
+In this exercise we are going to take a text and then count word frequencies in the text. Then the goal is to return the top most frequent word. 
+I plan to use a sorting algorithm then return the first three elements.
+'''
+
+import re
