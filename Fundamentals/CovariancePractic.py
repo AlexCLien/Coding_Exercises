da94c0f1659419bd2725c48df34e774c800bcac8
Alexander Lien <alexclien@Alexanders-MacBook-Air.local>
2026-09-07 16:02:38 -0700

    Second iteration, removed x-y unpacking constraint by multiplying through each array

--- Fundamentals/CovariancePractic.py
+++ Fundamentals/CovariancePractic.py
@@ -2,17 +2,31 @@
 This is a simple exercise to understanding covariance, one of the parts of the pca pipeline. Covariance measures how two embeddings vary together across review. 
 If one moves up will the other move down>
 '''
+import math
+
 data = [
     [ 2,  4],
     [-1, -2],
     [ 1,  2],
     [-2, -4]
-]
-products = [x * y for x,y in data] 
-sum_products = sum(products)
-print(sum_products)
+] #dataset
+'''
+First implementation 
 
-denominator = len(products) - 1
+products = [x * y for x,y in data]  #saves the sum of the two elements in the dataset into an array called products
+sum_products = sum(products) #adds all the elements in the list
+denominator = len(products) - 1 #takes the length and subtracts by 1
 
 cov = sum_products / denominator
 print(cov)
+'''
+
+products = [] #initialize array to store products
+for array in data: #this for look iterates through every array in the data and calculates the product of the elements. it then adds it to the products list
+    products += [math.prod(array)]
+sum_products = sum(products) #adds all the products todether
+denominator = len(products) - 1 #takes the length and subtracts by 1
+print(sum_products)
+cov = sum_products / denominator #calculates the covariance
+print(cov)
+
